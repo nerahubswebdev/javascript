f1e5b2cc9429379de4ce7257769333610d661f9e
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-05-08 08:09:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var name = "Miles";
+var age = 4;
+
+// datatypesin javascript
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Array
+// object
+
+//operators in javascript
+//  addition injavascript
+const sum = 2 + 3;
+
+console.log("the sum result is ", sum);
+
+const num1 = 6;
+const num2 = 7;
+
+const add = num1 + num2;
+const divide = num1 / num2;
+const mod = num1 % num2;
+
+const accessible = num1 > 30;
+
+console.log("the sum of num 1 and num2 is ", num1 + num2);
+console.log("the add result is ", add);
+
+//lets make statements with javascript variables
+const student_name = "West";
+const course = "Javascript class";
+
+const greeting = `Good morning student_name, welcome to course`;
+const greeting2 = "Good morning ${student_name}, welcome to ${course}";
+
+console.log(greeting);
+console.log(greeting2);
+
+// subtract 10 from num1 + num2
+
+//tell a students age from a variable and calculate the students savings for two days: Note savings for day 1 is a number type same as savings for day 2.age is also a number.use all details you have of the student to make a statement.
+
+const choose_number = parseInt(prompt("Enter your desired number"));
+console.log(choose_number);
+const choose_number2 = parseInt(prompt("Enter your desired number"));
+console.log(choose_number);
+console.log("the data type of choose_number is ", typeof choose_number);
+const result = choose_number + choose_number2;
+console.log("the sun is ", result);
