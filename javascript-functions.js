ddae3e1b13a3dd3a34f7f2defca44a4225b34f8e
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-05-20 11:58:46 +0100

    async await

--- javascript-functions.js
+++ javascript-functions.js
@@ -162,3 +162,91 @@ function average2(numberArray) {
 }
 
 average2(people);
+
+// the function asynchronous javascript
+const count = "Miles";
+
+let countValue = new Promise(function (resolve, reject) {
+  if (count === "Miles") {
+    resolve("There is a count value");
+  } else {
+    reject("There is no count value");
+  }
+});
+
+console.log("the count value is =>", countValue);
+
+// using then callback
+countValue.then((result) => {
+  console.log("the result => ", result);
+});
+
+//async await
+function fetchRandomData() {
+  const newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      {
+        const data = { success: false, message: "Miles name blabla" };
+
+        if (data?.success) {
+          resolve(data);
+        } else {
+          reject("It did not come.");
+        }
+      }
+    }, 5000);
+  });
+  return newPromise;
+}
+
+// the asyc await
+async function fetchData() {
+  try {
+    console.log("Fetching data ......");
+    const response = await fetchRandomData();
+    //console.log("the raw response => ", response);
+    return response;
+  } catch (error) {
+    //console.log("the response error => ", error);
+    throw error;
+  }
+}
+
+fetchData()
+  .then((blabla) => {
+    console.log("the result of the promise call => ", blabla);
+  })
+  .catch((error) => {
+    console.log("the fail safe => ", error);
+  });
+
+async function fetchActualData() {
+  try {
+    console.log("Fetching data ......");
+    const response = await fetch(
+      "https://cohort2-blog-api.onrender.com/post/all-posts"
+    );
+    //console.log("the raw response => ", response);
+    return response.json();
+  } catch (error) {
+    //console.log("the response error => ", error);
+    throw error;
+  }
+}
+
+fetchActualData()
+  .then((blabla) => {
+    console.log("the result of the promise call => ", blabla);
+
+    const lengthofposts = blabla?.posts.length;
+    const first5posts = blabla?.posts.slice(0, 5);
+    const lengthof5 = first5posts.length;
+    const title = blabla?.posts[1].title;
+
+    const prduct = lengthofposts * lengthof5;
+
+    // log out all the data u query
+  })
+  .catch((error) => {
+    console.log("the fail safe => ", error);
+  });
